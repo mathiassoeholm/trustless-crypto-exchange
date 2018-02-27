91d42e9e2d03c29713e412567f4c1dd0f0a6a02d
Mathias <mathiassoeholm@gmail.com>
2018-02-27 22:33:16 +0100

    Create initial UI with material design

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import ClippedDrawer from './components/ClippedDrawer';
+import Typography from 'material-ui/Typography';
+import 'typeface-roboto'
 import './App.css';
+
+const classes = {}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <ClippedDrawer classes={classes} title="Secure Ethereum Exchange">
+       <TextField
+          id="username"
+          label="User"
+          margin="normal"/>
+        <br/>
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+        />
+        <br/>
+        <Button variant="raised" color="primary">
+          Login
+        </Button>
+        </ClippedDrawer>
       </div>
     );
   }
