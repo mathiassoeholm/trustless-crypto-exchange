4a04cac50f044c369fa7025cba606fa5850d9326
Erik Høj Petersen <erikhoj.p@gmail.com>
2018-03-12 14:54:00 +0100

    some unit tests for flow reducer

---
+++ front-end/src/modules/flow/reducer.test.js
@@ -0,0 +1,51 @@
+import flowActionTypes from './actionTypes';
+import authActionTypes from '../auth/actionTypes';
+import menuTypes from './menuTypes';
+import reducer from './reducer';
+import deepFreeze from 'deep-freeze';
+
+it('changes menu to wallet when logging in and is pure', () =>
+{
+	const stateBefore = { activeMenu: menuTypes.LOGIN };
+	const stateAfter = { activeMenu: menuTypes.WALLET };
+
+	const action =
+	{
+		type: authActionTypes.LOG_IN
+	};
+
+	deepFreeze(stateBefore);
+
+	const newState = reducer(stateBefore, action);
+
+	expect(newState).toEqual(stateAfter);
+});
+
+it('changes menu to login after logging out', () =>
+{
+	const stateAfter = { activeMenu: menuTypes.LOGIN };
+
+	const action =
+	{
+		type: authActionTypes.LOG_OUT
+	};
+
+	const newState = reducer(undefined, action);
+
+	expect(newState).toEqual(stateAfter);
+});
+
+it('changes menu correctly', () =>
+{
+	const stateAfter = { activeMenu: menuTypes.WALLET };
+	
+	const action =
+	{
+		type: flowActionTypes.CHANGE_MENU,
+		menuType: menuTypes.WALLET
+	};
+
+	const newState = reducer(undefined, action);
+
+	expect(newState).toEqual(stateAfter);
+});
