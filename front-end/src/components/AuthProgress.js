3419ba5c688406d1b3497a9ff69cd989f8ef8703
Mathias <mathiassoeholm@gmail.com>
2018-03-12 15:50:44 +0100

    Added progress indicator

---
+++ front-end/src/components/AuthProgress.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import { LinearProgress } from 'material-ui/Progress';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+
+const AuthProgress = ({ title, open, progress, message }) =>
+{
+	return (
+		<Dialog open={open} fullWidth>
+			<DialogTitle id="simple-dialog-title">{title}</DialogTitle>
+			<Typography component="p" style={{padding: 10}}>
+				{message}
+			</Typography>
+			<LinearProgress />
+			<br />
+		</Dialog>
+	);
+};
+
+AuthProgress.propTypes =
+{
+	title: PropTypes.string.isRequired
+};
+
+const mapStateToProps = (state) =>
+({
+	open: state.auth.loginAttemptStatus && !state.auth.isLoggedIn,
+	progress: state.auth.loginAttemptStatus ? state.auth.loginAttemptStatus.progress : 0,
+	message: state.auth.loginAttemptStatus ? state.auth.loginAttemptStatus.message : ''
+});
+
+export default connect(mapStateToProps)(AuthProgress);
--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import UserForm from './UserForm';
 import authActions from '../modules/auth/actions';
 import { connect } from 'react-redux';
-
+import AuthProgress from './AuthProgress';
 
 const mapDispatchToProps = (dispatch) =>
 ({
@@ -12,7 +12,10 @@ const mapDispatchToProps = (dispatch) =>
 let Login = (props) =>
 {
 	return (
-		<UserForm {...props} buttonText="Login"/>
+		<div>
+			<UserForm {...props} buttonText="Login"/>
+			<AuthProgress title="Logging in"/>
+		</div>
 	);
 };
 
