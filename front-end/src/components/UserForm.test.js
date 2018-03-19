84ab8dacaf788f58cbac0068c3b759056f6b6773
Erik Høj Petersen <erikhoj.p@gmail.com>
2018-03-19 20:33:44 +0100

    callback test

--- front-end/src/components/UserForm.test.js
+++ front-end/src/components/UserForm.test.js
@@ -4,10 +4,11 @@ import { UserForm } from "./UserForm";
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 
-describe("UserForm", () =>
+describe('UserForm', () =>
 {
 	let props;
 	let mountedUserForm;
+	let buttonCallbackMock = jest.fn();
 
 	const userForm = () =>
 	{
@@ -25,7 +26,7 @@ describe("UserForm", () =>
 	{
 		props =
 		{
-			onClickedButton: () => {},
+			onClickedButton: buttonCallbackMock,
 			buttonText: 'Button',
 			username: 'Bob',
 			onChangedUsername: () => {},
@@ -34,20 +35,35 @@ describe("UserForm", () =>
 		mountedUserForm = undefined;
 	});
 
-	it("always renders a div", () =>
+	it('always renders a div', () =>
 	{
 		const divs = userForm().find("div");
 		expect(divs.length).toBeGreaterThan(0);
 	});
 
-	it("renders the username", () =>
+	it('renders the username', () =>
 	{
 		expect(userForm().find(TextField).first().props().value).toBe('Bob');
 	});
 
-	it("sets the button text", () =>
+	it('sets the button text', () =>
 	{
 		const button = userForm().find(Button);
 		expect(button.props().children).toBe(props.buttonText);
+	});
+
+	it('has a single password field', () =>
+	{
+		expect(userForm().find('input#password').length).toBe(1);
+	});
+
+	it('updates password field and calls callback correctly', () =>
+	{
+		const pwField = userForm().find('input#password').first();
+		pwField.simulate('change', { target: { value: 'pass' }});
+
+		userForm().find(Button).simulate('click');
+		expect(buttonCallbackMock.mock.calls.length).toBe(1);
+		expect(buttonCallbackMock.mock.calls[0][0]).toBe('pass');
 	});
 });
