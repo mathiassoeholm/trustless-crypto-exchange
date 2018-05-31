4de1981d1baa419e65a6ae15283a547d01059b69
Mathias <mathiassoeholm@gmail.com>
2018-05-31 13:34:54 +0200

    Changed some tests so new protocol can be added

---
+++ front-end/src/modules/auth/protocol/authenticated-protocol.js
@@ -0,0 +1,7 @@
+import config from '../../../config';
+import utils from './utils';
+
+export default (keyGen = config.keyGenerator, authApi = config.authApi) =>
+	({
+
+	});
--- front-end/src/modules/auth/protocol/protocols.test.js
+++ front-end/src/modules/auth/protocol/protocols.test.js
@@ -2,12 +2,22 @@ import each from 'jest-each';
 import utils from './utils';
 
 import simpleProtocol from './simpleProtocol';
+import authenticatedProtocol from './authenticated-protocol';
 import stubApi from '../api/stubApi';
 
 each(
 	[
-		['Simple Protocol', simpleProtocol],
-	]).describe('%s', (protocolName, chosenProtocol) =>
+		[
+			'Simple Protocol',
+			simpleProtocol,
+			[
+				{ username: 'kurt' }, // User
+				'start123', // Password
+				{ username: 'kurt' }, // Secret
+			],
+		],
+		//['Authenticated Protocol', authenticatedProtocol],
+	]).describe('%s', (protocolName, chosenProtocol, createArguments) =>
 {
 	let protocol;
 	const testKeyGenerator = utils.keyGenerator(1);
@@ -19,13 +29,13 @@ each(
 
 	it('creates a user', async () =>
 	{
-		await protocol.createUser({ username: 'kurt' }, 'start123', {});
+		await protocol.createUser(...createArguments);
 		expect(stubApi.getState().username).toEqual('kurt');
 	});
 
 	it('creates and stores secret', async () =>
 	{
-		await protocol.createUser({ username: 'kurt' }, 'start123', { username: 'kurt' });
+		await protocol.createUser(...createArguments);
 		const result = await protocol.login('kurt', 'start123');
 
 		expect(result.username).toEqual('kurt');
@@ -54,16 +64,10 @@ each(
 
 	it('encrypts secret correctly and stores on api when creating', async () =>
 	{
-		const username = 'bob';
+		const password = createArguments[1];
+		const secret = createArguments[2];
 
-		const secret =
-		{
-			username,
-		};
-
-		const password = 'password';
-
-		await protocol.createUser({ username }, password, secret);
+		await protocol.createUser(...createArguments);
 
 		const { salt } = stubApi.getState();
 		const key = await testKeyGenerator(password, salt);
