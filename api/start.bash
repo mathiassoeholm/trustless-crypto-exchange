8f9692d1f34b89a5e57eb19b8e395c1c0c634f38
Erik Høj Petersen <erikhoj.p@gmail.com>
2018-06-04 09:26:50 +0200

    start file

--- api/start.bash
+++ api/start.bash
@@ -1,6 +1,5 @@
-cd api
-yarn build-production
-yarn rename-production
-forever start -c "yarn production" ./
+sudo yarn build-production
+sudo yarn rename-production
+yarn production
 
 forever start -c "ganache-cli --defaultBalanceEther 9000000000000000000000 --db ./ganache-db" ./
