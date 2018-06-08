97a4723c0b3513db4240fbff4010712049f66b42
Erik Høj Petersen <erikhoj.p@gmail.com>
2018-06-08 12:18:05 +0200

    fixed warnings

--- front-end/src/config/default.js
+++ front-end/src/config/default.js
@@ -1,7 +1,7 @@
 import realAuthApi from '../modules/auth/api/real-api';
 import makeEthereumProvider from '../modules/wallet/provider/ethereum-provider';
-import makeStubProvider from '../modules/wallet/provider/stub-provider';
-import stubAuthApi from '../modules/auth/api/stub-api';
+// import makeStubProvider from '../modules/wallet/provider/stub-provider';
+// import stubAuthApi from '../modules/auth/api/stub-api';
 import makeProtocol from '../modules/auth/protocol/protocol';
 import utils from '../modules/auth/protocol/utils';
 import realWalletApi from '../modules/wallet/api/real-api';
