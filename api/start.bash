165f86731e70d10e9c8709d8922f07b9dceb39bc
Erik Høj Petersen <erikhoj.p@gmail.com>
2018-06-15 00:16:14 +0200

    fixed launch command

--- api/start.bash
+++ api/start.bash
@@ -1,4 +1,4 @@
 sudo yarn build-production;
 yarn production;
 
-forever start -c "sudo ganache-cli --defaultBalanceEther 9000000000000000000000 --db ./ganache-db" ./;
+forever start -c "sudo ganache-cli --account="0xd0553e1b1a82f39bffbc492c4538e5d3c8bd21f71fd2d360d5272e2a1e4e4fcc,9000000000000000000000"" ./
